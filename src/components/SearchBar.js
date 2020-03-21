5e22e83d68034cffa79a732a93f3896c9f82c76b
ThomasFeldman <tfeldman1.31@gmail.com>
2020-03-21 18:48:13 -0500

    Solving Context Issues

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = {term: 'hi there'};
     
-    onFormSubmit(event) {
+    onFormSubmit = (event) => {
         event.preventDefault();
 
         console.log(this.state.term);
